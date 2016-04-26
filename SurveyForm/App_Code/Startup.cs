13267d85fb964932c31cb95974386aad955da906
Paritosh Balasubrahmanyam <Paritosh Balasubrahmanyam>
2016-04-27 02:53:03 +1000

    adding files

---
+++ SurveyForm/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurveyForm.Startup))]
+namespace SurveyForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ SurveyForm/Default2.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //btnsubmit.ServerClick += btnsubmit_ServerClick;
+
+    }
+
+    void btnsubmit_ServerClick(object sender, EventArgs e)
+    {
+
+        if (!ValidateInputs()) ;
+
+    }
+
+    private bool ValidateInputs()
+    {
+        throw new NotImplementedException();
+    }
+
+}
