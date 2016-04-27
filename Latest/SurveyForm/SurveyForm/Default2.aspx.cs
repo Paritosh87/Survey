3e63f4247fb3c83069f1fe79af597ac643e47cee
Paritosh Balasubrahmanyam <paritosh.balasubrahmanyam@ingdirect.com.au>
2016-04-27 16:48:13 +1000

    message

---
+++ Latest/SurveyForm/SurveyForm/Default2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using SurveyForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private bool ValidateInputs()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void btnsubmit_Click(object sender, EventArgs e)
+    {
+        //if (!ValidateInputs()) ;
+
+        //m.Surveys.Add(s);
+        //m.SaveChanges();
+        using (var context = new masterEntities3())
+       
+        {
+            Survey s = new Survey();
+            {
+                s.VolunteerName = "Pari";
+            };
+            context.Surveys.Add(s);
+            context.SaveChanges();
+        }
+
+    }
+}
